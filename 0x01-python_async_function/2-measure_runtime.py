29af0a677c08708286e9be5052f61da03fa51016
Gold Israel <techiegold101@gmail.com>
2024-03-18 21:25:38 +0100

    add function that measures the avarage execution time of a coroutine function

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module that measure the average execution time for wait_n.
+
+Args:
+    n(int): Number of times to spawn wait_random.
+    max_delay(int): maximum delay in seconds.
+"""
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """ Measures the average execution time for wait_n(n, delay)."""
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+
+    return(end_time - start_time) / n
