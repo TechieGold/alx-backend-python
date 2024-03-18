81143ec702c52153c59189c65502d5dc7d347a08
Gold Israel <techiegold101@gmail.com>
2024-03-18 21:07:05 +0100

    import and use List object from typing

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,10 +8,11 @@ Returns:
     list: List of delays (float values) in ascending order.
 """
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Asynchronous routine that spawns wait_random n times """
     waits = await asyncio.gather(
         *tuple(map(lambda _: wait_random(max_delay), range(n)))
