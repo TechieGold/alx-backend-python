593f0b0fec10735c16924aa9ba7c1608471c0abb
Gold Israel <techiegold101@gmail.com>
2024-03-20 12:54:55 +0100

    function that returns sum of int and float numbers

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""6-sum_mixed_list.py"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """ Returns sum of mixed list"""
+    return sum(mxd_lst)
