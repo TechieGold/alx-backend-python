42d73148f91fe5e3a84db9cb3dffeabc30888de3
Gold Israel <techiegold101@gmail.com>
2024-03-20 13:14:18 +0100

    use Turple object from typing

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ 7-to_kv.py """
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: int | float) -> tuple:
+def to_kv(k: str, v: Union[int | float]) -> Tuple[str, float]:
     """ returns a tuple"""
-    return(k, v**2)
+    return(k, float(v ** 2))
