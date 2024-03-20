9e21a51dcb814798c8c3bc3719ade48ebc4d7349
Gold Israel <techiegold101@gmail.com>
2024-03-20 14:17:01 +0100

    now i have to use Sequence object from typing

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-""" 9-element_length.py """
-from typing import List, Tuple
+""" 9-element_length.py"""
+from typing import Iterable, List, Sequence, Tuple
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
-    """Returns the length of List element."""
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Computes the length of a list of sequences."""
     return [(i, len(i)) for i in lst]
