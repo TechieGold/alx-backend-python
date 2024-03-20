dc8c6c1fa2ad9d68f6adf8c529f64a7d7dfad28c
Gold Israel <techiegold101@gmail.com>
2024-03-20 14:27:32 +0100

    function that retrieves first element of a list

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,11 @@
+#!/usr/bin/envnpython3
+""" 100-safe_first_element.py """
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Returns the first element of a list safely."""
+    if lst:
+        return (lst[0])
+    else:
+        return None
